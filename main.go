8464d683d880fec351af399c4641de003b209a18
naol86 <naol.kasinet@a2sv.org>
2024-06-17 10:49:55 +0300

    test

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gofiber/fiber/v2";
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+);
 
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
@@ -12,7 +17,15 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", Hello)
+	app.Get("/env", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! from env" + os.Getenv("ENV"))
+	})
 
-	app.Listen("0.0.0.0:")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 
 }
